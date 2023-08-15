d7c74efa24899820b409ba21729895e06188310c
CarQuispe <carmen.quispe@ucb.edu.bo>
2023-08-15 00:37:52 -0400

    saludoNombre2

--- saludo.js
+++ saludo.js
@@ -1,5 +1,10 @@
-function saludar(a) {
-    return a;
+alert("Presiona OK si estas list@ para empezar con javascript");
+
+function saludarPorNombre(nombre) {
+    console.log(`¡Hola, ${nombre}! ¡Bienvenido!`);
   }
+  
+  // Llamada a la función
+  saludarPorNombre("Juan");
   
   export default saludar;
