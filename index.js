ee6e3c47ef8b5ce266f43113ef082d730bc4573e
CarQuispe <carmen.quispe@ucb.edu.bo>
2023-08-15 00:47:32 -0400

    saluCo4

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-import './saludo';
+// saludo.js
+export function saludar(nombre) {
+    console.log(`¡Hola, ${nombre}! ¡Bienvenido!`);
+  }
+  
+  // index.js
+  import { saludar } from './saludo';
+  
+  saludar("Juan");
