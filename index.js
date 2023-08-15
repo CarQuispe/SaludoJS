22a308a8ea7fabce4fcf7e9a839fd2c6c30cde17
CarQuispe <carmen.quispe@ucb.edu.bo>
2023-08-15 00:55:13 -0400

    SaludoCorregido

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// saludo.js
-export function saludar(nombre) {
-    console.log(`¡Hola, ${nombre}! ¡Bienvenido!`);
-  }
-  
+
   // index.js
   import { saludar } from './saludo';
   
--- saludo.js
+++ saludo.js
@@ -1,10 +1,6 @@
 alert("Presiona OK si estas list@ para empezar con javascript");
 
-function saludarPorNombre(nombre) {
+export function saludar(nombre) {
     console.log(`¡Hola, ${nombre}! ¡Bienvenido!`);
   }
   
-  // Llamada a la función
-  saludarPorNombre("Juan");
-  
-  export default saludar;
